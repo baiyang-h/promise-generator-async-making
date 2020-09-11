423bc0a0198b3b0a8f6fbfddc10c36a39a834b69
hhq <7900124+earthly_guest@user.noreply.gitee.com>
2020-09-11 15:40:46 +0800

    mod： 文档目录修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,18 +4,18 @@ module.exports = {
   themeConfig: {
     sidebar: [
       {
-        title: 'Group 1',   // 必要的
-        path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        title: '手写Promise',   // 必要的
+        // path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
         children: [
           '/'
         ]
       },
-      {
-        title: 'Group 2',
-        children: [ /* ... */ ]
-      }
+      // {
+      //   title: 'Group 2',
+      //   children: [ /* ... */ ]
+      // }
     ]
   }
 }
